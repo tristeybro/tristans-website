c4496133f750dfb48e6703e8f8b1e0155a68195f
tristeybro <the.tristanity.bena@gmail.com>
2017-08-30 17:46:34 -0400

    Fixing infocard

--- src/Components/InfoCard/InfoCard.js
+++ src/Components/InfoCard/InfoCard.js
@@ -6,14 +6,19 @@ const InfoCard = (props) => {
 	return (
 		<div className={styles.infocard}>
 			<img src={props.image}></img>
-			<div>{props.text}</div>
+			<div>
+				{props.text.map(x => {
+					return <p>{x}</p>;
+				})
+				}
+			</div>
 		</div>
 	)
 }
 
 InfoCard.propTypes = {
 	image: PropTypes.string.isRequired,
-	text: PropTypes.string.isRequired,
+	text: PropTypes.array.isRequired,
 };
 
 export default InfoCard;
--- src/Containers/InfoCardContainer/InfoCardContainer.js
+++ src/Containers/InfoCardContainer/InfoCardContainer.js
@@ -1,15 +1,14 @@
 import React from 'react';
 import InfoCard from '../../Components/InfoCard/InfoCard';
 
-const infoText = `
-I am a Brooklyn transplant from Los Angeles.
+const infoText = [`I am a Brooklyn transplant from Los Angeles.
 I write artisinal software made from the finest of keystrokes.
-My code creates a rich and vibrant tapestry of indentations that is scenic to behold.
-When I am not composing code, I like to repeatedly lift heavy objects from point A to point B.
+My code creates a rich and vibrant tapestry of indentations that is scenic to behold.`,
+`When I am not composing code, I like to repeatedly lift heavy objects from point A to point B.
 I also write other stuff that is meant to be read by humans.
 I can also play Jingle Bells on the guitar for the holidays.
 Avid consumer of politics, literature, and mathematics.
-`;
+`];
 
 const imageUrl = 'https://s3.amazonaws.com/thetristanity/img/headshot0.jpg';
 
