c678477ef421046199ba7f66ba028d7c9c10131c
tristeybro <the.tristanity.bena@gmail.com>
2017-12-31 18:50:53 -0500

    Prevent zoom controller from overlapping with tooltip

--- src/Api/api.js
+++ src/Api/api.js
@@ -3,11 +3,17 @@ import { ref } from '../Config/constants';
 export const fetchPosts = () => {
 	return ref.child('blog_post_metadata')
 		 				.once('value')
-     				.then((snapshot) => snapshot.val() || {})
+     				.then((snapshot) => snapshot.val() || {});
 };
 
 export const fetchPost = (id) => {
 	return ref.child(`blog_post_content/${id}`)
 						.once('value')
-						.then((snapshot) => snapshot.val() || {})
+						.then((snapshot) => snapshot.val() || {});
+};
+
+export const fetchLifeEvents = () => {
+	return ref.child('life_events')
+						.once('value')
+						.then((snapshot) => snapshot.val() || {});
 };
--- src/Containers/TimelineContainer/TimelineContainer.js
+++ src/Containers/TimelineContainer/TimelineContainer.js
@@ -2,25 +2,8 @@ import React from 'react';
 import Timeline from '../../Components/Timeline/Timeline';
 import Tooltip from '../../Components/Tooltip/Tooltip';
 import styles from './TimelineContainer.css';
+import { fetchLifeEvents } from '../../Api/api.js';
 import * as d3 from 'd3';
-
-const events = [
-	{date: "09/30/1990", description: "Born in Los Angeles, CA.", img: "https://s3.amazonaws.com/thetristanity/img/me_as_baby.jpg"},
-	{date: "11/01/2005", description: "Started wrestling in high school.", img: "https://s3.amazonaws.com/thetristanity/img/wrestling_minimalist_logo.jpg"},
-	{date: "05/12/2006", description: "Got our first house dog, subsequently named Chachy by my sister. Still have not gotten another dog named Joni.", img: "https://s3.amazonaws.com/thetristanity/img/chachy_lookback.jpg"},
-	{date: "08/12/2007", description: "Took my first shirtless mirror pic.", img: "https://s3.amazonaws.com/thetristanity/img/first_shirtless_mirror_pic.jpg"},
-	{date: "05/01/2008", description: "Graduated from Rancho Cucamonga High School.", img: "https://s3.amazonaws.com/thetristanity/img/rchs_badge.jpeg"},
-	{date: "05/01/2012", description: "Graduated from Stanford University", img: "https://s3.amazonaws.com/thetristanity/img/stanford.jpg"},
-	{date: "08/01/2012", description: "Moved to New York City", img: "https://s3.amazonaws.com/thetristanity/img/times_square.jpeg"},
-	{date: "09/01/2012", description: "Started working at OkCupid via Match Media Group", img: "https://s3.amazonaws.com/thetristanity/img/okcupid.jpg"},
-	{date: "05/01/2015", description: "Received a masters degree in computer science with a focus in machine learning from Columbia University", img: "https://s3.amazonaws.com/thetristanity/img/columbia_university.jpg"},
-	{date: "06/14/2015", description: "Started working at Shutterstock, not to be confused with Shutterfly. Never quite realized my dream of becoming a stock photography model.", img: "https://s3.amazonaws.com/thetristanity/img/spaghetti_stock_photo.jpg"},
-	{date: "09/01/2016", description: "Moved to a new apartment inside a church in Williamsburg. I now go to church every day.", img: "https://s3.amazonaws.com/thetristanity/img/spire_lofts.jpg"},
-	{date: "02/01/2017", description: "Started investing in cryptocurrencies. Coincidentally, also began to lament losing all those Bitcoins in college.", img: "https://s3.amazonaws.com/thetristanity/img/bitcoin.jpg"},
-	{date: "01/16/2017", description: "Visited Cuba in order to avoid the first week of Donald Trump's presidency.", img: "https://s3.amazonaws.com/thetristanity/img/riding_horses_in_cuba_mini.jpg"},
-	{date: "01/09/2017", description: "Started working at a startup called Google. Can now use the pick-up line: 'are you my employer because you are everything I am searching for.'", img: "https://s3.amazonaws.com/thetristanity/img/google_logo.jpg"},
-	{date: "09/15/2017", description: "Visited Europe for the first time. Went to Greece, Italy, Croatia, Austria and Germany.", img: "https://s3.amazonaws.com/thetristanity/img/me_at_oktoberfest.jpeg"}
-];
 
 class TimelineContainer extends React.Component {
 
@@ -34,6 +17,8 @@ class TimelineContainer extends React.Component {
 			height: 100 * 240,
 			timelineMargin: 10,
 			intervalDelta: 24,
+			isFetchingLifeEvents: true,
+			lifeEvents: [],
 		}
 	}
 
@@ -77,6 +62,8 @@ class TimelineContainer extends React.Component {
     									.style("display", "none")
     									.style("position", "absolute");
 
+    const zoomController = d3.select(this.state.zoomController);
+
 		const timePoints = this.getTimePoints(startDate, endDate, this.state.numIntervals);
 
 		const points = this.state.svg.append("g").selectAll("g")
@@ -97,7 +84,7 @@ class TimelineContainer extends React.Component {
 					.text((d) => d.toLocaleDateString("en-US"));
 
 		const lifeEvents = this.state.svg.append("g").selectAll("g")
-								.data(events)
+								.data(this.state.lifeEvents)
 								.enter()
 								.append("g")
 								.attr("transform", (evt) => `translate(100,${timeScale(new Date(evt.date).getTime())})`);
@@ -115,12 +102,14 @@ class TimelineContainer extends React.Component {
 		            tooltip.html(`<h4>${d.date}</h4>` + `<img src="${d.img}"></img>` + "<br><br>" + d.description)
 		            	     .style("left", (d3.event.pageX + 20) + "px")		
                 	     .style("top", (d3.event.pageY - 28) + "px");
+                zoomController.style("opacity", "0");
 		          })					
 			        .on("mouseout", (d) => {		
 			        	tooltip.transition()		
 	                     .duration(100)		
 	                     .style("opacity", 0)
 	                     .style("display", "none");
+	              zoomController.style("opacity", "1");
 			        });
 
 	}
@@ -149,6 +138,18 @@ class TimelineContainer extends React.Component {
 
 	handleScroll = (elem) => {
 		this.state.zoomController.style.top = `${elem.target.scrollTop}px`;
+	}
+
+	componentDidMount() {
+		if (this.state.isFetchingLifeEvents) {
+			fetchLifeEvents().then(val => {
+				this.setState({
+					isFetchingLifeEvents: false,
+					lifeEvents: Object.values(val),
+				});
+				this.renderTimeline();
+			});
+		}
 	}
 
 	render() {
