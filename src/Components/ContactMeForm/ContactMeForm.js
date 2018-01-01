c443a09b7c97c98ade07d2147fd9139be96b52ab
tristeybro <the.tristanity.bena@gmail.com>
2017-12-31 19:32:02 -0500

    Adding margin to sides of Contact Me form

--- src/Components/ContactMeForm/ContactMeForm.js
+++ src/Components/ContactMeForm/ContactMeForm.js
@@ -7,8 +7,8 @@ class ContactMeForm extends React.Component {
 			<form className={styles.contact_me_form}>
 				<label for="email">Email</label><br />
 				<input id="email" name="email" type="text" placeholder="Enter your email"></input><br />
-				<label for="subject">Subject</label><br />
-				<input id="subject" name="subject" type="text" placeholder="Enter the subject"></input><br />
+				<label for="subject">Subject</label>
+				<div><input id="subject" name="subject" type="text" placeholder="Enter the subject"></input></div>
 				<label for="message">Message</label><br />
 				<textarea id="message" name="message" placeholder="What's on your mind?"></textarea><br />
 				<button type="submit">Submit</button>
