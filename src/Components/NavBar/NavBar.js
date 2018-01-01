18412444b7c149906ba3c564a215cb74fc5520a3
tristeybro <the.tristanity.bena@gmail.com>
2017-12-31 21:48:19 -0500

    Making navbar mobile responsive

--- src/Components/NavBar/NavBar.js
+++ src/Components/NavBar/NavBar.js
@@ -3,28 +3,41 @@ import styles from './NavBar.css';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 
-const NavBar = (props) => {
-	return (
-		<div className={styles.navbar_container}>
+class NavBar extends React.Component {
 
-			<div className={styles.logo_container}>
-				<Link exact to='/'>
-					<img className={styles.navbar_image} src={props.image_src}></img>
-				</Link>
-				<div className={styles.hamburger_menu_container}>
-					<img src="https://s3.amazonaws.com/thetristanity/img/hamburger_menu_icon.png"></img>
+	constructor(props) {
+		super(props);
+		this.state = {
+			isNavbarShown: false,
+		};
+	}
+
+	toggleNavLinks = () => {
+		this.setState({ isNavbarShown: !this.state.isNavbarShown });
+	}
+
+	render() {
+		const navbarLinkClasses = this.state.isNavbarShown ? `${styles.navbar_link} ${styles.active}` : `${styles.navbar_link}`;
+		return (
+			<div className={styles.navbar_container}>
+
+				<div className={styles.logo_container}>
+					<Link exact to='/'>
+						<img className={styles.navbar_image} src={this.props.image_src}></img>
+					</Link>
+					<div className={styles.hamburger_menu_container}>
+						<img src="https://s3.amazonaws.com/thetristanity/img/hamburger_menu_icon.png" onClick={this.toggleNavLinks}></img>
+					</div>
 				</div>
+
+				<div className={navbarLinkClasses}><Link to='/bios'>BIO</Link></div>
+				<div className={navbarLinkClasses}><Link to='/posts'>POSTS</Link></div>
+				<div className={navbarLinkClasses}><Link to='/connect'>CONNECT</Link></div>
+
 			</div>
-
-			<div className={styles.navbar_link}><Link to='/bios'>BIO</Link></div>
-			<div className={styles.navbar_link}><Link to='/posts'>POSTS</Link></div>
-			<div className={styles.navbar_link}><Link to='/connect'>CONNECT</Link></div>
-
-
-
-		</div>
-	)
-};
+		)
+	};
+}
 
 NavBar.propTypes = {
 	image_src: PropTypes.string.isRequired,
